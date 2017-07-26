e792bdbd0ed5ed8e5f1cd7573a8bda983456cc7a
root <root@ip-172-31-28-175.ap-southeast-1.compute.internal>
2017-07-26 06:01:19 -0400

    updates

---
+++ update-cs.sh
@@ -0,0 +1,14 @@
+echo "Updating connection string"
+echo
+read -p "Please provide MySQL Server Host : " host
+#read -p "Please provide MySQL Server Port : " port
+#read -p "Please provide MySQL Server DB : " db
+#read -p "Please provide MySQL Server User : " user
+#read -p "Please provide MySQL Server Password : " password
+export MYSQL_SERVICE_HOST="$host"
+export MYSQL_SERVICE_PORT=3306
+export MYSQL_DATABASE=cardb
+export MYSQL_USER=admin
+export MYSQL_PASSWORD=Newuser#123
+echo
+echo "Connection string updated successfully."
